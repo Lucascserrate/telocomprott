b296b08136b5a67bbc4673303974d051e4fe263f
Lucas Camacho <Lucascserrate@gmail.com>
2023-05-25 01:25:58 -0400

    ruta fav

--- Main.jsx
+++ Main.jsx
@@ -2,6 +2,7 @@ import { View } from 'react-native'
 import { Routes, Route } from 'react-router-native'
 import HomeScreen from './screens/HomeScreen'
 import DetailScreen from './screens/DetailScreen'
+import FavoriteScreen from './screens/FavoriteScreen'
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <Routes>
         <Route path='/' element={<HomeScreen />} />
         <Route path='/:id' element={<DetailScreen />} />
+        <Route path='/fav' element={<FavoriteScreen />} />
       </Routes>
     </View>
   )
